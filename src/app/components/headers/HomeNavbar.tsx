2295513b9db161249940a03eb50feb274203846b
tillo01 <xuribayevzakaryo@gmail.com>
2024-07-10 08:45:36 +0900

    fix:modify  homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 export function HomeNavbar(){
     const authMember = null;
     return(
-        <div className="home-navbar">
-            <Container className="navbar-container">
-               <Stack className="menu">
+    <div className="home-navbar">
+    <Container className="navbar-container">
+        <Stack className="menu">
 
               <Box>
               <NavLink to={"/"}>
@@ -14,42 +14,55 @@ export function HomeNavbar(){
               </NavLink>
               </Box>
 
-              <Stack className="links">
-              <Box className={"hover-line"}>
-              <NavLink to={"/"} activeClassName="underline">Home</NavLink>
-              </Box>
-              <Box className={"hover-line"}>
-              <NavLink to={"/products"} activeClassName="underline">Products</NavLink>
-              </Box>
-              {authMember ? (
-                <Box className={"hover-line"}>
-                <NavLink to={"/orders"} activeClassName="underline">Orders</NavLink>
-                </Box>
-              ) :null}
-              {authMember ? (
-                <Box className={"hover-line"}>
-                <NavLink to={"/member-page"} activeClassName="underline">My Page</NavLink>
-                </Box>
-              ) :null}
-              <Box className={"hover-line"}>
-              <NavLink to={"/help"} activeClassName="underline">Help</NavLink>
-              </Box>
+            <Stack className="links">
+                    <Box className={"hover-line"}>
+                    <NavLink to={"/"} activeClassName="underline">Home</NavLink>
+                    </Box>
+                    <Box className={"hover-line"}>
+                    <NavLink to={"/products"} activeClassName="underline">Products</NavLink>
+                    </Box>
+                    {authMember ? (
+                        <Box className={"hover-line"}>
+                        <NavLink to={"/orders"} activeClassName="underline">Orders</NavLink>
+                        </Box>
+                    ) :null}
+                    {authMember ? (
+                        <Box className={"hover-line"}>
+                        <NavLink to={"/member-page"} activeClassName="underline">My Page</NavLink>
+                        </Box>
+                    ) :null}
+                    <Box className={"hover-line"}>
+                    <NavLink to={"/help"} activeClassName="underline">Help</NavLink>
+                    </Box>
 
-              {/* BASKET */}
+                    {/* BASKET */}
 
-              {!authMember ? (<Box><Button variant="contained" className="login-button">Login</Button></Box>) : (
-                
-                <img className="user-avatar" src="/icons/default-user.svg" aria-haspopup={true}/>)}
+                    {!authMember ? (<Box><Button variant="contained" className="login-button">Login</Button></Box>) : (
+                        
+                        <img className="user-avatar" src="/icons/default-user.svg" aria-haspopup={true}/>)}
 
-              </Stack>
-              
+            </Stack>
+        </Stack>
 
+        <Stack className="header-frame">
+                    <Stack className="detail">
+                         <Box className="head-main-txt">World`s Most Delicous Cousine</Box>
+                         <Box className="wel-txt">The Choice, not just a choice</Box>
+                         <Box className="service-txt">24 hours service</Box>
+                         <Box className="sign-up">
+                            {!authMember ? (
+                                <Button variant={"contained"} className="signup-button">SIGN UP</Button>)
+                             : null}
+                         </Box>
+                    </Stack>
 
-               
+                    <Box className="logo-frame">
+                        <div className="logo-img"></div>
 
-               </Stack>
-            </Container>
-        </div>
+                    </Box>
+        </Stack>
+        </Container>
+    </div>
     );
 }
 
