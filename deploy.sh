f7637edc0a459fdcfde879f01fd3660d2428c5c8
Tillochik01 <xuribayevzakaryo@gmail.com>
2025-01-12 05:54:55 +0900

    fix: deploy shell.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #PRODUCTION
-
 git reset --hard
 git checkout master
 git pull origin master
 
+npm i yarn -g
+yarn global add serve 
+yarn 
+yarn run build
 
 
 docker compose up -d
